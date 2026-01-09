bd37f319d062328986e6e47ae50e89d4814070ba
seoyogi <yuta0808ishibashi@gmail.com>
2026-01-10 07:07:54 +0900

    Update and rename main.py to webcraft.py

--- webcraft.py
+++ webcraft.py
@@ -1,27 +1,15 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
-from OpenGL.GLU import *
-
-angle = 0
 
 def draw():
-    global angle
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
     glTranslatef(0,0,-5)
-    glRotatef(angle,1,1,0)
-    glutWireCube(1)
-    angle += 1
+    glutSolidCube(1)
     glutSwapBuffers()
-
-def timer(v):
-    glutPostRedisplay()
-    glutTimerFunc(16, timer, 0)
 
 glutInit()
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-glutCreateWindow(b"Python 3D Game")
+glutCreateWindow(b"PythonCraft")
 glEnable(GL_DEPTH_TEST)
 glutDisplayFunc(draw)
-glutTimerFunc(0, timer, 0)
 glutMainLoop()
